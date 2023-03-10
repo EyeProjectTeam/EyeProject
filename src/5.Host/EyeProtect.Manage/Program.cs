c5b0c0b0dcfba9dd10fdba9ce66063681584af99
yanghongyu <1063671581@qq.com>
2023-03-10 10:17:43 +0800

    fix

--- src/5.Host/EyeProtect.Manage/Program.cs
+++ src/5.Host/EyeProtect.Manage/Program.cs
@@ -1,70 +1,15 @@
-using EyeProtect.Manage;
-using Serilog.Events;
-using Serilog;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
 using System;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using NLog.Extensions.Logging;
 using NLog;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
-using System.Xml.Linq;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
 using System.Text;
 
-//public class Program
-//{
-//    public async static Task<int> Main(string[] args)
-//    {
-//        try
-//        {
-//            var builder = WebApplication.CreateBuilder(args);
-//            builder.Host
-//                .ConfigureAppConfiguration((context, config) =>
-//                {
-//                    var env = context.HostingEnvironment.EnvironmentName;
-//                    // Õ¾µãÅäÖÃ
-//                    config
-//                        .AddJsonFile("host.json", true, true)
-//                        .AddJsonFile("appsettings.json", true, true)
-//                        .AddJsonFile($"appsettings.{env}.json", true, true)
-//                        .AddEnvironmentVariables();
-//                    if (args != null) config.AddCommandLine(args);
-//                })
-//                .ConfigureLogging((context, logging) =>
-//                {
-//                    var config = context.Configuration;
-//                    logging.AddNLog();
-//                    logging.AddDebug();
-//                    logging.AddEventSourceLogger();
-
-//                    logging.AddConfiguration(config.GetSection("Logging"));
-//                    LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
-//                })
-//                .UseNLog()
-//                .UseAutofac();
-//            await builder.AddApplicationAsync<EyeProtectManagerApiModule>();
-//            var app = builder.Build();
-//            await app.InitializeApplicationAsync();
-
-//            await app.RunAsync();
-//            return 0;
-//        }
-//        catch (Exception ex)
-//        {
-//            Console.WriteLine($"{ex.ToString()}-{ex.StackTrace}");
-//            return 1;
-//        }
-//        finally
-//        {
-//            Log.CloseAndFlush();
-//        }
-//    }
-//}
 namespace EyeProtect.Manage
 {
     public class Program
